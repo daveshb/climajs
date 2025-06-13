fda6225a633393e241d4aed96a8877b1eb3caccf
David Henao <daves.hb@hotmail.com>
2025-06-13 07:06:10 -0500

    add styles

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-const apiKey = 'xxxxxxxxxxxxxxxxx';
+
+const apiKey = "5b5b731343003351e1780f61e7a57c21";
+
+
+
+// const apiKey2 = process.env.MI_API_KEY;
+// console.log(apiKey2);
+
+console.log(process.env.MI_API_KEY)
+
+
 
 async function getWeather() {
-//   const city = document.getElementById('cityInput').value;
-  const city = "medellin"
-  const resultDiv = document.getElementById('result');
+
+
+  const cityInput = document.getElementById("city-input");
+  const city = cityInput ? cityInput.value.trim() : "";
+
+  const resultDiv = document.getElementById("result");
 
   if (!city) {
-    resultDiv.innerText = 'Por favor ingresa una ciudad.';
+    resultDiv.innerText = "Por favor ingresa una ciudad.";
     return;
   }
 
   try {
-    resultDiv.innerText = 'Buscando...';
+    resultDiv.innerText = "Buscando...";
 
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=es`
     );
 
-    if (!response.ok) throw new Error('Ciudad no encontrada.');
+    if (!response.ok) throw new Error("Ciudad no encontrada.");
 
     const data = await response.json();
     const { name, main, weather } = data;
 
-    console.log(data)
+    console.log(data.sys.sunset);
 
     resultDiv.innerHTML = `
       <p><strong>Ciudad:</strong> ${name}</p>
@@ -30,9 +43,8 @@ async function getWeather() {
       <p><strong>Clima:</strong> ${weather[0].description}</p>
     `;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     resultDiv.innerText = error.message;
   }
 }
 
-getWeather()
